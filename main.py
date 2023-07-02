368de92d47231ae7c5005e5e43e6d13e33e42e5f
Eiichi Takebuchi <nanashi4129@gmail.com>
2023-07-02 16:48:53 +0900

    トーンバーに関する特許を反映させる

--- main.py
+++ main.py
@@ -26,27 +26,54 @@ def getG(wn, zeta):
 def getCriticalDamping(m, w0):
     return 2. * m * w0
 
-def getDampingRatio(c, cc):
+def getZeta(c, cc):
     return c / cc
+
+# meters
+def getBarLength(f0):
+    return np.sqrt(0.5596/f0)
 
 if __name__ == "__main__":
     fa = 55.
-    za = 0.2
+    ma = 0.1
     wa = 2. * np.pi * fa
+    cca = getCriticalDamping(ma, wa)
+    za = getZeta(14., cca)
     Ga = getG(wa, za)
+    print(getBarLength(44))
 
     fb = 220.
-    zb = 0.9
+    mb = 0.01
     wb = 2. * np.pi * fb
+    ccb = getCriticalDamping(mb, wb)
+    zb = getZeta(27., ccb)
     Gb = getG(wb, zb)
 
     Gf = ctrl.feedback(Ga, Gb)
 
     print(Gf)
 
-    t = np.arange(0, 2, 0.01)
+    t = np.arange(0, 4, 0.01)
     y, t = ctrl.impulse(Gf, T=t)
 
     plt.figure()
     plt.plot(t, y)
     plt.show()
+
+"""
+Tone generator with vibratory bars
+
+https://patentimages.storage.googleapis.com/5b/1a/f6/13bfe158f6a3e8/US3644656.pdf
+
+捩じれたトーンバーに関する特許
+f0 = 0.5596/L^2 = \sqrt{QK^2 / p}
+L: 長さ
+Q: ヤング率
+K: 断面ねじりモーメント?
+p: 密度
+
+トーンバーの長さを求めるには、
+1/f0 = L^2/0.5596
+0.5596/f0 = L^2
+L = sqrt(0.5596/f0)
+"""
